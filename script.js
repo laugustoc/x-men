6c863fbd7a934ba35806d027a8b0126769ec493e
Lucas Souza <120069689+laugustoc@users.noreply.github.com>
2023-08-18 17:07:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // sempre pensar,planejar e descrever o que vc quer que o seu código faça antes de começar a codar. Colocar comentários no seu código para que você saiba o que cada parte doo seu código vai fazer.
 // NUNCA ESQUECER DA PORRA DO PONTO E VÍRGULA;
-
 const seletorPersonagem = document.querySelectorAll ('.personagem');
-
+const somSelecionar = document.querySelector('.select')
 // forEach = pra cada. É um laço de repetição for. Ou seja, eu quero armazenar cada personagem na lista personagens
 seletorPersonagem.forEach((personagem) => {
     personagem.addEventListener("mouseenter", ()=> {
+
+        somSelecionar.play();
 
         const personagemSelecionado = document.querySelector ('.selecionado');
         personagemSelecionado.classList.remove('selecionado')
@@ -27,3 +28,7 @@ seletorPersonagem.forEach((personagem) => {
 if(window.innerHeight < 450) {
     window.scrollTo({top:0, behavior: 'smooth'});
 }
+
+
+
+
